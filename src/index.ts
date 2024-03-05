e8b51ee454d2d6cfea50b3137b31a28fb8b0d85f
Your Name <you@example.com>
2024-03-05 16:32:56 +0100

    Question 4

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,37 @@
 import { readFileSync } from "fs";
 
+
+
 interface Person {
   age:number
+  height:number
 }
 
-function getStatistics():number {
+interface StatisticsResult {
+  meanAge: number;
+  meanHeight: number;
+}
+
+function getStatistics():StatisticsResult {
   const persons:Person[] = JSON.parse(readFileSync("./persons.json").toString());
   if (persons.length === 0) {
     throw new Error("Le fichier persons.json est vide.");
   }
 
-  let maxAge = persons[0].age;
+  let totalAge = 0;
+  let totalHeight = 0;
 
   for (const person of persons) {
-    if (person.age > maxAge) {
-      maxAge = person.age;
-    }
+    totalAge += person.age;
+    totalHeight += person.height;
   }
 
-  return maxAge;
+  const meanAge = totalAge / persons.length;
+  const meanHeight = totalHeight / persons.length;
+
+  return { meanAge, meanHeight };
 }
+
 
 function displayResult() {
   console.log(getStatistics());
