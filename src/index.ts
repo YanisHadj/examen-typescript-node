7b41f0f27a7eb9829104a898bf8564c6094ee22b
Your Name <you@example.com>
2024-03-05 16:20:40 +0100

    Question 3

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,24 @@
 import { readFileSync } from "fs";
 
-
-
 interface Person {
-  
+  age:number
 }
-
 
 function getStatistics():number {
   const persons:Person[] = JSON.parse(readFileSync("./persons.json").toString());
-  return 0
+  if (persons.length === 0) {
+    throw new Error("Le fichier persons.json est vide.");
+  }
+
+  let maxAge = persons[0].age;
+
+  for (const person of persons) {
+    if (person.age > maxAge) {
+      maxAge = person.age;
+    }
+  }
+
+  return maxAge;
 }
 
 function displayResult() {
